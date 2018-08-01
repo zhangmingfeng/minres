f15bfed3ca278808f79eab256b30635a6cdb9039
zhangmingfeng <zhangmf2008@126.com>
2018-08-01 19:02:44 +0800

    增加远程文件入库

--- controllers/fetch/fetch.go
+++ controllers/fetch/fetch.go
@@ -11,7 +11,6 @@ import (
 	"github.com/zhangmingfeng/minres/utils"
 	"net/http"
 	"net/url"
-	"runtime/debug"
 	"strconv"
 	"strings"
 )
@@ -31,8 +30,7 @@ func (f *Fetch) Fetch(w http.ResponseWriter, r *http.Request) {
 	f.ParseForm(r, &request)
 	defer func() {
 		if err := recover(); err != nil {
-			fmt.Println(err)
-			debug.Stack()
+			f.Logger().Error(err)
 			f.TextResponse(w, http.StatusNotFound, "not found")
 		}
 	}()
@@ -44,13 +42,17 @@ func (f *Fetch) Fetch(w http.ResponseWriter, r *http.Request) {
 	height := request.Height
 	mode := request.Mode
 	prefixList := make([]string, 0)
+	args := url.Values{}
 	if width > 0 {
+		args.Set("width", strconv.Itoa(width))
 		prefixList = append(prefixList, fmt.Sprintf("w%d", width))
 	}
 	if height > 0 {
+		args.Set("height", strconv.Itoa(height))
 		prefixList = append(prefixList, fmt.Sprintf("h%d", height))
 	}
 	if len(mode) > 0 {
+		args.Set("mode", mode)
 		prefixList = append(prefixList, fmt.Sprintf("m%s", mode))
 	}
 	prefix := strings.Join(prefixList, "_")
@@ -58,10 +60,6 @@ func (f *Fetch) Fetch(w http.ResponseWriter, r *http.Request) {
 	if len(prefix) > 0 {
 		fileMeta = f.CacheValue(fmt.Sprintf("%s_%s_meta", fid, prefix))
 	}
-	args := url.Values{}
-	args.Set("width", strconv.Itoa(width))
-	args.Set("height", strconv.Itoa(height))
-	args.Set("mode", mode)
 	var fileInfo *weed.FileInfo
 	var err error
 	if len(fileMeta) <= 0 {
@@ -70,7 +68,7 @@ func (f *Fetch) Fetch(w http.ResponseWriter, r *http.Request) {
 			panic(err)
 		}
 		// 仅仅缩略图才缓存文件
-		if !strings.HasPrefix(fileInfo.Mime, "image") && len(prefix) > 0 {
+		if strings.HasPrefix(fileInfo.Mime, "image") && len(prefix) > 0 {
 			err = minres.WeedClient.SaveFile(fmt.Sprintf("%s_%s", fid, prefix), fileInfo.GetData())
 			if err != nil {
 				panic(err)
@@ -80,8 +78,10 @@ func (f *Fetch) Fetch(w http.ResponseWriter, r *http.Request) {
 				panic(err)
 			}
 			f.Cache(fmt.Sprintf("%s_%s_meta", fid, prefix), string(val), 0)
+			f.Logger().Debug("save cache", val)
 		}
 	} else {
+		f.Logger().Debug("from cache", fileMeta)
 		fileInfo = &weed.FileInfo{}
 		err := utils.Json2Struct(fileMeta, fileInfo)
 		if err != nil {
